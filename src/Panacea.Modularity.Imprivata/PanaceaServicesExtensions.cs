cb2b07ad55b6629ae89fea1c3c751e0e8eba8cc4
Kostas Aronis <k.aronis@dotbydot.gr>
2019-05-27 15:30:44 +0300

    Empty Interface

---
+++ src/Panacea.Modularity.Imprivata/PanaceaServicesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Panacea.Core;
+using System.Linq;
+
+namespace Panacea.Modularity.Imprivata
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static IImprivataPlugin GetImprivata(this PanaceaServices core)
+        {
+            return core.PluginLoader.GetPlugin<IImprivataPlugin>();
+        }
+
+        public static bool TryGetImprivata(this PanaceaServices core, out IImprivataPlugin plugin)
+        {
+            plugin = null;
+            var imprivata = core.PluginLoader.GetPlugins<IImprivataPlugin>().FirstOrDefault();
+            if (imprivata == null)
+            {
+                return false;
+            }
+            plugin = imprivata;
+            return true;
+        }
+    }
+}
